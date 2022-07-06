20f0e09fcb379ac0d47418e37a60d32bdec4fa32
AJCordova <archonist252@gmail.com>
2022-07-06 17:01:33 +0800

    Set-up project to use a main coordinator and remove storyboard usage.
    Include rxswift frameworks and snapkit.

---
+++ EmployeeManager/EmployeeManager/AppCoordinator.swift
@@ -0,0 +1,39 @@
+//
+//  AppCoordinator.swift
+//  EmployeeManager
+//
+//  Created by COLLABERA on 7/6/22.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+protocol Coordinator {
+    func start()
+    func coordinate(to coordinator: Coordinator)
+}
+
+extension Coordinator {
+    func coordinate(to coordinator: Coordinator) {
+        coordinator.start()
+    }
+}
+
+final class AppCoordinator: Coordinator {
+    let window: UIWindow
+    let navigationController = UINavigationController()
+
+    init(window: UIWindow) {
+        self.window = window
+    }
+
+    func start() {
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+
+        let viewController = ViewController()
+        navigationController.pushViewController(viewController, animated: true)
+    }
+}
+
