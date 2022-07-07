0df20b5b17fedcfe34992d63d2f9a3dd6b2f3617
AJCordova <archonist252@gmail.com>
2022-07-07 12:00:44 +0800

    Create data models.

---
+++ EmployeeManager/EmployeeManager/Models/Employee.swift
@@ -0,0 +1,14 @@
+//
+//  Employee.swift
+//  EmployeeManager
+//
+//  Created by COLLABERA on 7/7/22.
+//
+
+import Foundation
+
+struct Employee: Codable, Equatable {
+    var id: UUID = UUID()
+    var name: String
+    var isEmployed: Bool
+}
---
+++ EmployeeManager/EmployeeManager/Models/Employer.swift
@@ -0,0 +1,13 @@
+//
+//  Employer.swift
+//  EmployeeManager
+//
+//  Created by COLLABERA on 7/7/22.
+//
+
+import Foundation
+
+struct Employer {
+    var name: String?
+    var employees: [Employee]?
+}
