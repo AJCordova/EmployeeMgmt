72931a26934ea5dc0ebda50f2868128880b18693
AJCordova <archonist252@gmail.com>
2022-07-07 23:10:34 +0800

    Add CurrentEmployer object.

---
+++ EmployeeManager/EmployeeManager/Services/CurrentEmployer.swift
@@ -0,0 +1,12 @@
+//
+//  CurrentUser.swift
+//  EmployeeManager
+//
+//  Created by COLLABERA on 7/7/22.
+//
+
+import Foundation
+
+struct CurrentEmployer {
+    var employer = SampleData.employerSample //Employer() // Empty Employer object
+}
